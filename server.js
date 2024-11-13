9656e85aed0b180d0a2c2953fd3b25f9eb4e0c2c
EngineerMoore <alexiashalise@gmail.com>
2024-11-13 14:33:59 -0600

    Routers and API get and post requests created

--- server.js
+++ server.js
@@ -2,30 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+})
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-const employees = require("./employees");
+app.use(`/employees`, require(`./API/employees`));
 
-app.get("/employees", (req, res) => {
-  res.json(employees);
-});
+app.use((req, res, next) => {
+  next({status: 404, message: `Sorry, endpoint does not exist`});
+})
 
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    res.status(404).send(`There is no employee with id ${id}.`);
-  }
-});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message);
+})
 
 app.listen(PORT, () => {
   `Listening on port ${PORT}...`;
